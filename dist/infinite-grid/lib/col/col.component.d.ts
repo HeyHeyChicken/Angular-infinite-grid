5e4fbcff6d9e634270d19360253283543cf1722a
Antoine Duval <antoine@cuffel.fr>
2022-10-19 20:40:23 +0200

    new release

--- dist/infinite-grid/lib/col/col.component.d.ts
+++ dist/infinite-grid/lib/col/col.component.d.ts
@@ -8,10 +8,16 @@ export declare class ColComponent implements OnInit {
     lg: number | 'auto';
     xl: number | 'auto';
     xxl: number | 'auto';
+    text_xs?: 'start' | 'center' | 'end';
+    text_sm?: 'start' | 'center' | 'end';
+    text_md?: 'start' | 'center' | 'end';
+    text_lg?: 'start' | 'center' | 'end';
+    text_xl?: 'start' | 'center' | 'end';
+    text_xxl?: 'start' | 'center' | 'end';
     protected row: RowComponent;
     classAttribute: string;
     constructor(parent: RowComponent);
     ngOnInit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ColComponent, [{ host: true; }]>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ColComponent, "column", never, { "xs": "xs"; "sm": "sm"; "md": "md"; "lg": "lg"; "xl": "xl"; "xxl": "xxl"; }, {}, never, ["*"], false>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ColComponent, "column", never, { "xs": "xs"; "sm": "sm"; "md": "md"; "lg": "lg"; "xl": "xl"; "xxl": "xxl"; "text_xs": "text_xs"; "text_sm": "text_sm"; "text_md": "text_md"; "text_lg": "text_lg"; "text_xl": "text_xl"; "text_xxl": "text_xxl"; }, {}, never, ["*"], false>;
 }
